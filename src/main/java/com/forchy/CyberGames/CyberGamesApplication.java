ff9617a7a8669ea069e3dd484e57f173876b0198
F0rchy <theartemka6@mail.ru>
2022-09-22 20:48:43 +0300

    Beginning

---
+++ src/main/java/com/forchy/CyberGames/CyberGamesApplication.java
@@ -0,0 +1,13 @@
+package com.forchy.CyberGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forchy/CyberGames/CyberGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forchy.CyberGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
